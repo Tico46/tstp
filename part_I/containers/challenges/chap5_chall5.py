26d86a1ac17c67d7cf0662abafe528ff6a92ec88
Tico46 <35152383+Tico46@users.noreply.github.com>
2018-01-25 16:12:18 -0500

    Songs are not in lists
    Proposing a change to add brackets to song names so that it indicates that these songs are in lists instead of values to dictionary keys. This way, users can add more songs. Thoughts?

--- part_I/containers/challenges/chap5_chall5.py
+++ part_I/containers/challenges/chap5_chall5.py
@@ -1,4 +1,4 @@
-songs = {"John Lennon": "Stand by Me",
-         "Kanye West": "Homecoming",
-         "Swedish House Mafia": "Don't You Worry Child"
+songs = {"John Lennon": ["Stand by Me"],
+         "Kanye West": ["Homecoming"],
+         "Swedish House Mafia": ["Don't You Worry Child"]
 }
